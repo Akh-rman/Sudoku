6254ef3f34b17bb34d1047fcabd3c677fd6e0aee
Rman <rman@Mac-Rman.local>
2017-04-20 12:57:57 +0300

    Написал функцию для меню выпадающего слева.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-$(function () { //Функция для сворачивания меню когда фокус переходит на другие элементы
+/*$(function () { //Функция для сворачивания меню когда фокус переходит на другие элементы
     $(".navbar-toggle").blur(function (event) {
         var screenWidth = window.innerWidth;
         if (screenWidth < 768) {
             $("#bs-example-navbar-collapse-1").collapse('hide');
         }
     });
+});*/
+
+jQuery.fn.LeftToggle = function (duration, easing, complete) {
+    return this.animate({ width: "toggle"}, jQuery.speed(duration, easing, complete));
+};
+
+$(".navbar-toggle").click(function() { // функция для выпадающего меню слева
+    $("#navMenu").LeftToggle(200);
 });
-
-function showSize () {
-    var size = window.innerWidth;
-    console.log(size);
-}
-
-addEventListener("resize", showSize);
